b578a4d8093b35aecbe41d509ba770a747a3e71b
sarhatabaot <sarhatabaot1@gmail.com>
2019-05-05 17:28:49 +0300

    Updated dependecy & depracated methods
    * Updated towny dependency.
    * Updated old deprecated methods.

--- src/me/mrCookieSlime/CSCoreLibPlugin/protection/modules/TownyProtectionModule.java
+++ src/me/mrCookieSlime/CSCoreLibPlugin/protection/modules/TownyProtectionModule.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 
 import com.palmergames.bukkit.towny.object.TownyPermission.ActionType;
 import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
-import com.palmergames.bukkit.util.BukkitTools;
 
 public class TownyProtectionModule implements ProtectionModule {
 
@@ -15,12 +14,12 @@ public class TownyProtectionModule implements ProtectionModule {
     public boolean canBuild(Player player, Block b) {
     	//ActionType is DESTROY because other plugins mostly use canBuild function to check if the player has permission to destroy block.
     	//TODO: Split canBuild() into 2 functions (canBuild and canDestroy).
-        return PlayerCacheUtil.getCachePermission(player, b.getLocation(), BukkitTools.getTypeId(b), BukkitTools.getData(b), ActionType.DESTROY);
+        return PlayerCacheUtil.getCachePermission(player, b.getLocation(), b.getType(), ActionType.DESTROY);
     }
 
     @Override
     public boolean canAccessChest(Player player, Block b) {
-        return PlayerCacheUtil.getCachePermission(player, b.getLocation(), BukkitTools.getTypeId(b), BukkitTools.getData(b), ActionType.SWITCH);
+        return PlayerCacheUtil.getCachePermission(player, b.getLocation(), b.getType(), ActionType.SWITCH);
     }
 
     @Override
